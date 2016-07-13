b3043eb616f9373a6c44515c9e4d0e7322c12a3a
Jinesh George <jineshgeorge@gmail.com>
2016-07-13 07:25:40 -0700

    Basic roman add test

---
+++ roman_math_lib.c
@@ -0,0 +1,9 @@
+// File: roman_calc_lib.c 
+// Description: Roman arithemetic operations
+
+//Function to add two roman numbers
+char* add(char* first, char* second)
+{
+
+    return "II";
+}
---
+++ roman_math_lib.h
@@ -0,0 +1,5 @@
+// File: roman_calc_lib.h 
+// Description: Roman arithemetic operations
+
+// Function to add two roman numbers
+char* add(char* , char* );
